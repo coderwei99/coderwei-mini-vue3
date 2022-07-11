b5745e9359ff221411bf4d9328d50b8dfc3f6832
coderwei <916606569@qq.com>
2022-07-11 11:06:06 +0800

    feat: 新增isReadonly函数

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -5,6 +5,10 @@ import { createReactiveObject } from "./baseHandlers";
 export function createGetter<T extends object>(isReadonly = false) {
   return function get(target: T, key: string | symbol) {
     const res = Reflect.get(target, key);
+    if (key === "__v_isReadonly") {
+      return isReadonly;
+    }
+
     // track
     if (!isReadonly) {
       track(target, key);
@@ -12,6 +16,7 @@ export function createGetter<T extends object>(isReadonly = false) {
     return res;
   };
 }
+
 export function createSetter<T extends object>() {
   return function set(target: T, key: string | symbol, val: any) {
     const res = Reflect.set(target, key, val);
@@ -46,4 +51,14 @@ export function reactive<T extends object>(raw: T) {
 //readonly对象
 export function readonly<T extends object>(raw: T) {
   return createReactiveObject<T>(raw, readonlyHandlers);
+}
+
+// 为value类型做批注，让value有属性可选，必选使用的时候提示value没有xxx属性
+export interface ITarget {
+  __v_isReadonly?: boolean;
+}
+
+// 判断是否是一个只读对象
+export function isReadonly<T extends object>(value: unknown) {
+  return !!(value as ITarget)["__v_isReadonly"];
 }
--- src/reactive/test/readonly.spec.ts
+++ src/reactive/test/readonly.spec.ts
@@ -1,19 +1,28 @@
-import { readonly } from "../reactive";
+import { readonly, isReadonly } from "../reactive";
 
 describe("readonly", () => {
   it("readonly not set", () => {
     let original = {
       foo: {
         fuck: {
-          name: "i don't care",
+          name: "what",
         },
       },
       arr: [{ color: "#fff" }],
     };
     let warper = readonly(original);
     expect(warper).not.toBe(original);
-    expect(warper.foo.fuck.name).toBe("i don't care");
+    expect(isReadonly(warper)).toBe(true);
+    expect(isReadonly(original)).toBe(false);
+    // 测试嵌套对象的reactive状态
+    // expect(isReadonly(warper.foo.fuck)).toBe(true);
+    // // expect(isReadonly(warper.foo.fuck.name)).toBe(true)
+    // // 因为name是一个基本类型所以isObject会是false，暂时对name生成不了readonly，涉及到往后的知识点 isRef
+    // expect(isReadonly(warper.arr)).toBe(true);
+    // expect(isReadonly(warper.arr[0])).toBe(true);
+    expect(warper.foo.fuck.name).toBe("what");
   });
+
   it("warning when it be call set operation", () => {
     let original = {
       username: "ghx",
