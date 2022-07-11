1e1fb13f7852dcbbef4fd7c657d32ddaf4d338c0
coderwei <916606569@qq.com>
2022-07-11 09:15:34 +0800

    refactor: 对响应式木块代码进行重构

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -2,27 +2,33 @@ import { track, trigger } from "./effect";
 
 import { createReactiveObject } from "./baseHandlers";
 
-// reactive响应式对象的handle捕获器
-export const mutableHandlers: ProxyHandler<object> = {
-  get(target, key: string | symbol) {
+export function createGetter<T extends object>(isReadonly = false) {
+  return function get(target: T, key: string | symbol) {
     const res = Reflect.get(target, key);
-    // 收集依赖
-    track(target, key);
+    // track
+    if (!isReadonly) {
+      track(target, key);
+    }
     return res;
-  },
-  set(target, key: string | symbol, val: any) {
+  };
+}
+export function createSetter<T extends object>() {
+  return function set(target: T, key: string | symbol, val: any) {
     const res = Reflect.set(target, key, val);
-    // 触发依赖
     trigger(target, key);
     return res;
-  },
+  };
+}
+
+// reactive响应式对象的handle捕获器
+export const mutableHandlers: ProxyHandler<object> = {
+  get: createGetter(),
+  set: createSetter(),
 };
 
 // readonly只读对象的handle捕获器
 export const readonlyHandlers: ProxyHandler<object> = {
-  get(target, key) {
-    return Reflect.get(target, key);
-  },
+  get: createGetter(true),
   set(target, key, val) {
     console.warn(
       `${target} do not set ${String(key)} value ${val}, because it is readonly`
