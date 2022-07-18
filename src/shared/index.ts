4b706e05da72f3217623a91c5f76e9d53f39bf3d
coderwei <916606569@qq.com>
2022-07-18 10:26:51 +0800

    feat: 添加isFunction判断函数

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,8 @@
 export function isObject(value: unknown) {
   return typeof value !== null && typeof value === "object";
 }
+
+// 判断是否是一个函数
+export function isFunction(value: unknown) {
+  return typeof value == "function";
+}
