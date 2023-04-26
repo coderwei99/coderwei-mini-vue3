36ae1e3b7fe41e3bb58d3cde6b78aafb93073b22
coderwei <916606569@qq.com>
2023-04-27 03:46:00 +0800

    fix: 对一个watchEffect同时触发多次依赖进行去重

--- packages/runtime-core/src/scheduler.ts
+++ packages/runtime-core/src/scheduler.ts
@@ -2,6 +2,7 @@ const queue: any[] = []
 let activePreFlushCbs: any[] = []
 let activePostFlushCbs: any[] = []
 let showExecte = false
+let isFlushing = false
 
 export function nextTick(fn?: () => void) {
   return fn ? Promise.resolve().then(fn) : Promise.resolve()
@@ -25,6 +26,7 @@ function queueFlush() {
 
 function flushJobs() {
   showExecte = false
+  isFlushing = false
 
   /** 增加个判断条件，因为目前为止，我们视图的异步渲染和watchEffect的异步执行 都是走到这个位置，而在这里watchEffect的第二个参数的flush是pre的时候，需要在视图更新之前执行
       所以我们可以先在这里执行我们收集起来的需要在视图更新之前执行的函数
@@ -63,6 +65,8 @@ export function queuePosstFlushCb(fn) {
 }
 
 function queueFns(fn: any, activePreFlushCbs: any[]) {
+  if (isFlushing) return
+  isFlushing = true
   activePreFlushCbs.push(fn)
   queueFlush()
 }
