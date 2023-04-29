92f2271472cc8758039064e2cf088579a93535e4
coderwei <916606569@qq.com>
2023-04-29 08:50:33 +0800

    fix: 修复更新组件函数异常

--- packages/runtime-core/src/componentUpdateUtils.ts
+++ packages/runtime-core/src/componentUpdateUtils.ts
@@ -1,6 +1,11 @@
 export function shouldUpdateComponent(n1: any, n2: any) {
   const { props: preProps } = n1
   const { props: nextProps } = n2
+
+  if (n1.children !== n2.children) {
+    return true
+  }
+
   for (const key in nextProps) {
     if (preProps[key] != nextProps[key]) {
       return true
