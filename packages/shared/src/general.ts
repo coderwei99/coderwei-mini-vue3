7a4bd0130ca800bb0420306ec00dd45aa0f085f6
coderwei <916606569@qq.com>
2023-05-30 02:13:02 +0800

    fix(shared): isObject函数兼容是null的情况

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -1,6 +1,6 @@
 // 判断是否为一个对象
 export function isObject(value: unknown) {
-  return typeof value !== null && typeof value === 'object'
+  return value !== null && typeof value === 'object'
 }
 
 // 判断是否是一个函数
